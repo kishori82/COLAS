e2defd9c5aeda206d11dcbbc8d503cf15a0fe589
kishori <kishori82@gmail.com>
2016-08-24 04:24:01 +0000

    Added the Go and C ABD server

--- src/abd/go/server.go
+++ src/abd/go/server.go
@@ -5,14 +5,31 @@ import (
 	"fmt"
 	//	"os"
 )
+/*
+#cgo CFLAGS: -I../C
+#cgo LDFLAGS: -L../C  -labd_shared -lzmq -lczmq
+#include <abd_server.h>
+*/
+import "C"
+
+func server_daemon() {
+	active_chan = make(chan bool)
+
+	data.active = true
+	data.name = "server_1"
+
+
+	C.ABD_server_process( C.CString(data.name), C.CString(data.port) )
+
+
+}
 
 func Server_process() {
-
 	fmt.Println("Starting server\n")
 	f := SetupLogging()
 	defer f.Close()
-	//go HTTP_Server()
 	// Run the server for now
-	HTTP_Server()
+	go HTTP_Server()
+ server_daemon()
 
 }
