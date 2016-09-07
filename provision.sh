338803f046637af270cb7ba4f4a78633074449d7
kishori82 <kishori82@gmail.com>
2016-09-07 11:26:07 -0400

    bug fixes ABD

--- provision.sh
+++ provision.sh
@@ -1,6 +1,9 @@
 curl -L 172.17.0.1:8080/SetName/reader_1
+
 curl -L 172.17.0.1:8080/SetServers/172.17.0.2
 curl -L 172.17.0.2:8080/SetServers/172.17.0.2
+
 curl -L 172.17.0.2:8080/SetName/server_1
+
 curl -L 172.17.0.1:8080/StartProcess
 curl -L 172.17.0.2:8080/StartProcess
