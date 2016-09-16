2d89d99e1a0aaea72e3f797c5482ada971049c20
kishori82 <kishori82@gmail.com>
2016-09-16 13:49:50 -0400

    changed the algorithm to reed_solomon in the writer

--- server.sh
+++ server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #~/COLAS/src/abdprocess --process-type 2
-~/COLAS/src/abdprocessc --process-type 2  --ip  172.17.0.2 --ip 172.17.0.3 --ip 172.17.0.4  --filesize  50  --wait 100  --algorithm SODAW  --code rlnc 
+~/COLAS/src/abdprocessc --process-type 2  --ip  172.17.0.2 --ip 172.17.0.3 --ip 172.17.0.4  --filesize  50  --wait 100  --algorithm SODAW  --code reed_solomon 
 #--serverid $1
