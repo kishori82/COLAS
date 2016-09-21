84b4b18bd510c56746712af750044b466ae13f9e
kishori82 <kishori82@gmail.com>
2016-09-21 10:37:33 -0400

    switching the server.sh to use 3 servers ip addresses

--- server.sh
+++ server.sh
@@ -3,7 +3,7 @@
 #~/COLAS/src/abdprocessc --process-type 2  --ip  172.17.0.2 --ip 172.17.0.3 --ip 172.17.0.4  --filesize  50  --wait 100  --algorithm SODAW  --code reed_solomon 
 #--serverid $1
 #~/COLAS/src/abdprocessc --process-type 2  --ip  172.17.0.2 --filesize  4  --wait 100  --algorithm SODAW  --code reed_solomon 
-#~/COLAS/src/abdprocessc --process-type 2  --ip  172.17.0.2 --ip  172.17.0.3 --ip  172.17.0.4 --filesize  4  --wait 100  --algorithm SODAW  --code reed_solomon 
+~/COLAS/src/abdprocessc --process-type 2  --ip  172.17.0.2 --ip  172.17.0.3 --ip  172.17.0.4 --filesize  4  --wait 100  --algorithm SODAW  --code reed_solomon 
 #--serverid $1
 
 
@@ -12,6 +12,6 @@
 
 
 # DAVID
-valgrind --leak-check=full --tool=memcheck --suppressions=vg.supp   ~/COLAS/src/abdprocessc --process-type 2  --ip  172.17.0.1 --filesize  4  --wait 100  --algorithm ABD  --code reed_solomon 
+#valgrind --leak-check=full --tool=memcheck --suppressions=vg.supp   ~/COLAS/src/abdprocessc --process-type 2  --ip  172.17.0.1 --filesize  4  --wait 100  --algorithm ABD  --code reed_solomon 
 #valgrind --tool=massif ~/COLAS/src/abdprocessc --process-type 2  --ip  172.17.0.1 --filesize  4  --wait 100  --algorithm ABD  --code reed_solomon 
 #valgrind --tool=callgrind ~/COLAS/src/abdprocessc --process-type 2  --ip  172.17.0.1 --filesize  4  --wait 100  --algorithm SODAW --code reed_solomon 
