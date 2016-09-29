ad5da15599249309075f1baed7850de0f50df582
kishori <kishori82@gmail.com>
2016-09-29 16:30:48 +0000

    Added 3 servers

--- server.sh
+++ server.sh
@@ -19,4 +19,4 @@
 #valgrind --tool=callgrind ~/COLAS/src/abdprocessc --process-type 2  --ip  172.17.0.1 --filesize  4  --wait 100  --algorithm SODAW --code reed_solomon 
 
 #gdb -ex run  --args ~/COLAS/src/abdprocessc --process-type 2  --ip  172.17.0.1 --ip  172.17.0.3 --ip  172.17.0.4 --filesize  4  --wait 100  --algorithm SODAW  --code reed_solomon 
- ~/COLAS/src/abdprocessc --process-type 2  --ip  172.17.0.1 --ip  172.17.0.3 --ip  172.17.0.4 --filesize  4  --wait 100  --algorithm SODAW  --code reed_solomon 
+ ~/COLAS/src/abdprocessc --process-type 2  --ip  172.17.0.1 --ip  172.17.0.2 --ip  172.17.0.3 --filesize  4  --wait 100  --algorithm SODAW  --code reed_solomon 
