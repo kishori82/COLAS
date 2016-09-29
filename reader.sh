d05dd6fc6bf9b939d85fd8524590c3d6c9354974
kishori <kishori82@gmail.com>
2016-09-29 18:39:19 +0000

    Fixed a problem of sending the opnum as a addres in sending the multicast at the reader

--- reader.sh
+++ reader.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #~/COLAS/src/abdprocess --process-type 0
-~/COLAS/src/abdprocessc --process-type 0  --ip  172.17.0.2 --ip 172.17.0.3 --ip 172.17.0.4  --filesize  4  --wait 100  --algorithm SODAW  --code reed_solomon 
+gdb -ex run --args ~/COLAS/src/abdprocessc --process-type 0  --ip  172.17.0.1 --ip 172.17.0.2 --ip 172.17.0.3  --filesize  4  --wait 100  --algorithm SODAW  --code reed_solomon 
 #--serverid $1
