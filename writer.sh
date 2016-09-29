0255d63aa8fee104ffacaeecae3d5ae37e188436
kishori <kishori82@gmail.com>
2016-09-29 16:27:04 +0000

    removed the decoding check

--- writer.sh
+++ writer.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #~/COLAS/src/abdprocess --process-type 1
 #!/bin/bash
-~/COLAS/src/abdprocessc --process-type 1  --ip  172.17.0.2 --ip 172.17.0.3 --ip 172.17.0.4  --filesize  4  --wait 100  --algorithm SODAW  --code reed_solomon 
+ ~/COLAS/src/abdprocessc --process-type 1  --ip  172.17.0.1 --filesize  4  --wait 100  --algorithm SODAW  --code reed_solomon 
+#~/COLAS/src/abdprocessc --process-type 1  --ip  172.17.0.2 --ip 172.17.0.3 --ip 172.17.0.4  --filesize  4  --wait 100  --algorithm SODAW  --code reed_solomon 
 #~/COLAS/src/abdprocessc --process-type 1  --ip  172.17.0.2 --ip 172.17.0.3  --filesize  4  --wait 100  --algorithm SODAW  --code reed_solomon 
 #~/COLAS/src/abdprocessc --process-type 1  --ip  172.17.0.2 --ip  172.17.0.3 --ip  172.17.0.4 --filesize  4  --wait 100  --algorithm SODAW  --code reed_solomon 
 #~/COLAS/src/abdprocessc --process-type 1  --ip  172.17.0.1 --filesize  4  --wait 100  --algorithm ABD  --code reed_solomon 
@@ -11,5 +12,5 @@
 
 
 # TEST
-valgrind --leak-check=full --tool=memcheck --show-leak-kinds=all  ~/COLAS/src/abdprocessc --process-type 1  --ip  172.17.0.2 --ip 172.17.0.3 --ip 172.17.0.4  --filesize  4  --wait 100  --algorithm SODAW  --code reed_solomon 
+#valgrind --leak-check=full --tool=memcheck --show-leak-kinds=all  ~/COLAS/src/abdprocessc --process-type 1  --ip  172.17.0.2 --ip 172.17.0.3 --ip 172.17.0.4  --filesize  4  --wait 100  --algorithm SODAW  --code reed_solomon 
 #~/COLAS/src/abdprocessc --process-type 1  --ip  172.17.0.2 --ip 172.17.0.3 --ip 172.17.0.4  --filesize  4  --wait 100  --algorithm SODAW  --code reed_solomon 
