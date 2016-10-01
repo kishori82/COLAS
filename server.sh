9c19639feb99391debf718ae2123d5e97ebdb549
kishori <kishori82@gmail.com>
2016-10-01 20:55:10 +0000

    Fixed memory leak in ABD server

--- server.sh
+++ server.sh
@@ -19,4 +19,4 @@
 #valgrind --tool=callgrind ~/COLAS/src/abdprocessc --process-type 2  --ip  172.17.0.1 --filesize  4  --wait 100  --algorithm SODAW --code reed_solomon 
 
 #gdb -ex run  --args ~/COLAS/src/abdprocessc --process-type 2  --ip  172.17.0.1 --ip  172.17.0.3 --ip  172.17.0.4 --filesize  4  --wait 100  --algorithm SODAW  --code reed_solomon 
- ~/COLAS/src/abdprocessc --process-type 2  --ip  172.17.0.2  --filesize  4  --wait 100  --algorithm ABD  --code reed_solomon 
+valgrind --leak-check=full --tool=memcheck   ~/COLAS/src/abdprocessc --process-type 2  --ip  172.17.0.2  --filesize  4  --wait 100  --algorithm ABD  --code reed_solomon 
